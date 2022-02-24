c8a0ef058a5a59036debbb08729497a002eef87f
AlekseyLu <luzyanin-ae@yandex.ru>
2022-02-24 15:42:27 +0300

    Немного красоты

--- src/HiButton/Hi_button.jsx
+++ src/HiButton/Hi_button.jsx
@@ -4,14 +4,20 @@ import Random from './Dop.jsx';
 
 function Hibutton() {
     return (
-      <div className="ButtonClick">
-        <button onClick={() => {
-          Random()
-          console.log(' ')}
-          }>
-          Самая доброжелательная кнопка!
-        </button>
-      </div>
+      <body>
+          <div className="social instagram">
+            <p className="slova">Нажми на иконку inst</p>
+              <a href="https://www.instagram.com/aleksey_lu/" target="_blank"><i className="fa fa-instagram fa-5x"></i></a>
+                <div className="ButtonClick">
+                  <button onClick={() => {
+                    Random()
+                    console.log(' ')}
+                    }>
+                    Button
+                  </button>
+                </div>
+          </div>
+      </body> 
     );
   }
   export default Hibutton;
