ff76fed4728cb663f8214b5a0e30285454b7da4e
Andrew <81930648+Tireless13@users.noreply.github.com>
2022-03-09 15:16:08 +0500

    cleaning

--- src/AndrewCustomButton/AndrewCustomButton.jsx
+++
@@ -1,9 +0,0 @@
-export default function AndrewCustomButton({ children, compliment, complimentCount }) {
-    const onClick = () => {
-        for (let i = 0; i < complimentCount; i++) {
-            console.log(compliment);
-            console.log('---------');
-        }
-    }
-    return (<button onClick={onClick}>{children}</button>);
-}
--- src/HiButton/Dop.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function Random() {
-    const arr = ['Hi', 'Hello', 'Privet', 'Zdorova', 'Good morning', 'ХУЁРНИНГ!', 'Have a good day'];
-    const random = Math.floor(Math.random()* arr.length);
-    console.log(arr[random]);
-}
